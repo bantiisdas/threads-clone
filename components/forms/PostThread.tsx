bf3195b5b5a4eddd55068730ddfd6583aebbc691
Supriya Das <bantibuto3@gmail.com>
2023-09-15 19:13:23 +0530

    Almost Done - Some Fetaures need to be implemented

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -17,6 +17,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { usePathname, useRouter } from "next/navigation";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
+import { useOrganization } from "@clerk/nextjs";
 // import { updateUser } from "@/lib/actions/user.actions";
 // import { UserValidation } from "@/lib/validations/user";
 
@@ -37,6 +38,7 @@ function PostThread({ userId }: { userId: string}){
 
   const router = useRouter();
   const pathname = usePathname();
+  const { organization } = useOrganization();
 
   const form = useForm({
     resolver: zodResolver(ThreadValidation),
@@ -50,7 +52,7 @@ function PostThread({ userId }: { userId: string}){
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
 
